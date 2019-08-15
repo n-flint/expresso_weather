41306f4b0896a0bbca8dc48a507d5aca0a4813bc
Noah Flint <noah.flint24@gmail.com>
2019-08-15 15:35:43 -0600

    commented out account creation post request spec

--- app.spec.js
+++ app.spec.js
@@ -22,20 +22,16 @@ describe('api', () => {
     });
   });
 
-  // describe('Test POST /api/v1/users path', () => {
-  //   test('should return a 201 status', () => {
-  //     return request(app).post("/api/v1/users").then(response => {
-  //       expect(response.status).toBe(200)
-  //     });
-  //   });
-  //   test('should return an array of game objects', () => {
-  //     return request(app).get("/api/v1/games").then(response => {
-  //       expect(response.body.length).toEqual(4),
-  //         expect(Object.keys(response.body[0])).toContain('title')
-  //       expect(Object.keys(response.body[0])).toContain('price'),
-  //         expect(Object.keys(response.body[0])).toContain('releaseYear'),
-  //         expect(Object.keys(response.body[0])).toContain('active')
-  //     })
-  //   });
-  // });
+  describe('Test POST /api/v1/users path', () => {
+    test('should return a 201 status', () => {
+      return request(app).post("/api/v1/users").then(response => {
+        expect(response.status).toBe(201)
+      });
+    });
+    test('should return a users api key', () => {
+      return request(app).post("/api/v1/users", ).then(response => {
+          expect(Object.keys(response.body[0])).toContain('api_key')
+      })
+    });
+  });
 });
